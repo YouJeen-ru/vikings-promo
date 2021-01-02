7c3f15581aefb7e92fcb15386fc3d59e06dc9071
YouJeen-ru <1.rouch@mail.ru>
2021-01-02 10:57:20 +0300

    Adaptive layout completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    })
+
     $('[data-fancybox]').fancybox({
         youtube : {
             controls : 2,
@@ -14,6 +18,14 @@ $(function(){
         asNavFor: '.heroes__slider-text',
         prevArrow: '<button  class="slick-btn slick-prev"><img src="../images/arrow-left.png" alt="arrowL"></button>',
         nextArrow: '<button  class="slick-btn slick-next"><img src="../images/arrow-right.png" alt="arrowR"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
     $('.heroes__slider-text').slick({
         slidesToShow: 1,
